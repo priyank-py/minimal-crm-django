2815816987a0a98bd1cb27ac5c70872edb1b5255
Priyank <ipriyank007@hotmail.com>
2020-01-16 14:19:41 +0530

    Update settings.py

--- real_project/settings.py
+++ real_project/settings.py
@@ -92,22 +92,22 @@ WSGI_APPLICATION = 'real_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'pgtestcrmdb',
-#         'USER': 'postgres',
-#         'PASSWORD': 'password123',
-#         'HOST': 'localhost',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pgtestcrmdb',
+        'USER': 'postgres',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost',
+    }
+}
 
 
 # Password validation
